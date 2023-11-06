e2f3a460da52db2de73246d833ba9540a62affc8
Abdulrahman <abdulrahman1ye@gmail.com>
2023-11-06 20:57:46 +0300

    add images to each genocides

--- src/views/Genocide.vue
+++ src/views/Genocide.vue
@@ -12,15 +12,23 @@
 
       <p class="text-body  mt-1 text-justify">{{ $t('genocide.details') }}</p>
 
-      <div>
-        <div v-for="(section, index) in tm('genocide.cides')" :key="index">
 
-          <h1 class="text-h6 font-weight-bold ">{{ rt(section.title) }}</h1>
-          <p class="text-subtitle-2 font-weight-bold mb-1">{{ rt(section.date) }}</p>
-          <p class="text-body" v-html="rt(section.details)"></p>
+      <div v-for="(section, index) in  tm('genocide.cides') " :key="index">
 
-        </div>
+        <article>
+          <header class="mx-auto max-w-screen-xl pt-28 text-center">
+            <p class="text-gray-500">{{ rt(section.date) }}</p>
+            <h1 class="mt-2 text-3xl font-bold text-gray-900 sm:text-5xl">{{ rt(section.title) }}</h1>
 
+            <img v-if="imageExists[index]" class="responsive-image" :src="'images/genocides/' + index + '.jpg'"
+              :alt="rt(section.title)" />
+          </header>
+
+          <div class="mx-auto mt-10 max-w-screen-md space-y-12 px-4 py-10 font-serif text-lg tracking-wide text-gray-700">
+            <strong class="text-2xl font-medium text-justify" v-html="rt(section.details)"></strong>
+
+          </div>
+        </article>
       </div>
 
     </v-responsive>
@@ -29,13 +37,57 @@
 
 <script>
 import { useI18n } from 'vue-i18n'
-
 export default {
+  data() {
+    return {
+      imageExists: {},
+    };
+  },
+  methods: {
+    async checkImageExists(index) {
+      try {
+        const response = await fetch(`images/genocides/${index}.jpg`);
+        this.imageExists[index] = response.ok;
+      } catch (error) {
+        this.imageExists[index] = false;
+      }
+    },
+  },
+  created() {
+    // Call checkImageExists for each image index
+    for (let index = 0; index < this.tm('genocide.cides').length; index++) {
+      this.checkImageExists(index);
+    }
+  },
   setup() {
     const { tm, rt } = useI18n();
-    return { rt, tm }
+    const getImageUrl = (name) => {
+      console.log(new URL(`../../assets/genocides/${name}.jpg`, import.meta.url).toString())
+      return new URL(`../../assets/genocides/${name}.jpg`, import.meta.url).href
+    }
+    return { rt, tm, getImageUrl }
+
+
+
   }
 }
 
 
 </script>
+
+<style>
+.responsive-image {
+  width: 100%;
+  /* Default width for smaller screens */
+  height: auto;
+  /* Maintain aspect ratio */
+}
+
+@media (min-width: 768px) {
+
+  /* Set larger width for screens with a minimum width of 768px (e.g., tablets and desktops) */
+  .responsive-image {
+    width: 800px;
+    height: 450px;
+  }
+}</style>
